db0ce1e58bf82c4dc79c281123060fd8014f3589
LHMinhTrung <lehaminhtrung1010@gmail.com>
2024-11-01 13:15:08 +0700

    Remove any type

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -42,7 +42,7 @@ const Home = () => {
     );
   };
 
-  const swapArray = (array: any) => {
+  const swapArray = (array: string[]) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
